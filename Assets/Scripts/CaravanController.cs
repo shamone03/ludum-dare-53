ed3c3780fd7023a46cfcedb15dc3ed247c7bc8b3
Aryah <ar341476@dal.ca>
2023-04-30 10:51:49 -0300

    added center of mass object to car, will be used later, updated wheel transforms for caravan

--- Assets/Scripts/CaravanController.cs
+++ Assets/Scripts/CaravanController.cs
@@ -8,12 +8,20 @@ public class CaravanController : MonoBehaviour {
     [SerializeField] private WheelCollider frontLeftCollider;
     [SerializeField] private WheelCollider frontRightCollider;
 
-    
-    private void Start() {
-        
+    [Header("Wheel Transforms")] 
+    [SerializeField] private Transform frontLeftTransform;
+    [SerializeField] private Transform frontRightTransform;
+    private void UpdateWheelTransforms(WheelCollider wheelCollider, Transform wheelTransform) {
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos, out rot);
+        wheelTransform.position = pos;
+        wheelTransform.rotation = rot;
     }
-
+    
     private void Update() {
+        UpdateWheelTransforms(frontLeftCollider, frontLeftTransform);
+        UpdateWheelTransforms(frontRightCollider, frontRightTransform);
         frontLeftCollider.motorTorque = 0.0001f;
         frontRightCollider.motorTorque = 0.0001f;
     }
