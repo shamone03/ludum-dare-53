ecee7fd0120b4c948697e4e61d794b85d8b7daf5
Tristan <tristanjamesduke@gmail.com>
2023-04-30 18:40:50 -0400

    Man can now take damage inside caravan
    His head now acts as collision detection, only heavy impacts to the head will influence health.

--- Assets/Scripts/HealthBarScripts/Patient.cs
+++ Assets/Scripts/HealthBarScripts/Patient.cs
@@ -7,38 +7,42 @@ public class Patient : MonoBehaviour
     [SerializeField] private int maxHealth = 100;
     [SerializeField] public int currentHealth;
 
+
     private HealthBar healthBar;
 
     void Start() {
         StartCoroutine(DamagePatient());
         currentHealth = maxHealth;
-        healthBar = UIController.instance.healthSlider.GetComponent<HealthBar>();
-        healthBar.SetMaxHealth(maxHealth);
+        //healthBar = UIController.instance.healthSlider.GetComponent<HealthBar>();
+        //healthBar.SetMaxHealth(maxHealth);
     }
 
-    // void Update()
-    // {
-    //     //Test Code
-    //     if (Input.GetKeyDown(KeyCode.Space))
-    //     {
-    //         TakeDamage(10);
-    //     }
-    // }
-    void TakeDamage(int damage) {
+    void Update()
+    {
+        //nothing to do
+    }
+     void FixedUpdate()
+    {
+
+    }
+
+    public void TakeDamage(int damage)
+    {
+        //call grunt noise
         currentHealth -= damage;
-        // healthBar.SetHealth(currentHealth);
-        // if (currentHealth <= 0) {
-        //     // StopCoroutine(DamagePatient());
-        //     Destroy(this.gameObject);
-        // }
-    }
+        //healthBar.SetHealth(currentHealth);
 
-    IEnumerator DamagePatient() {
-        while (true) {
-            Debug.Log("Patient Hurt");
-            TakeDamage(10);
-            yield return new WaitForSeconds(1);
+        if(currentHealth <= 0)
+        {
+            //end game / round / reset / lose / whatever we're doing for this
         }
     }
-
+    IEnumerator DamagePatient() {
+        while (true) {
+            //Debug.Log("Patient Hurt");
+            TakeDamage(1);
+            yield return new WaitForSeconds(5);
+        }
+    }
+    
 }
